b1bddc2a0132db8086180d6b204b21fa2cfd7a3c
Jay Vardhan Chaudhary <88651830+jayvardhan12@users.noreply.github.com>
2024-02-24 16:20:07 +0530

    Add files via upload

---
+++ dominant_colours/CHANGELOG.md
@@ -0,0 +1,64 @@
+# Changelog
+
+## v1.1.8 - 2022-10-24
+
+More internal refactoring to use newer versions of kmeans-colors and palette.
+
+This has no feature changes.
+
+## v1.1.7 - 2022-10-24
+
+Some internal refactoring to use a newer versions of clap.
+
+This has no feature changes.
+
+## v1.1.6 - 2022-10-23
+
+Provide precompiled binaries for more targets, so the following targets are now supported:
+
+* Macs on Intel and Apple Silicon
+* Windows on Intel
+* Linux on Intel, both GNU and MUSL (Alpine-compatible) binaries
+
+There are no feature/bugfix changes from v1.1.3.
+
+(Note: v1.1.4 and v1.1.5 were abortive releases to get the new binaries working, and have been removed to avoid confusion.
+
+## v1.1.3 - 2022-10-17
+
+Fix a bug when finding the dominant colour of some animated GIFs.
+
+## v1.1.2 - 2022-10-16
+
+Add support for TIFF images.
+
+## v1.1.1 - 2022-04-03
+
+Publish binaries as part of the GitHub releases.
+
+This has no feature changes.
+
+## v1.1.0 - 2021-11-29
+
+Allow finding the dominant colour of GIFs.
+
+This includes animated GIFs, and dominant_colours looks at multiple frames.
+
+## v1.0.1 - 2021-11-28
+
+Use uppercase text for variables in the help text.
+i.e.
+
+```
+dominant_colours <PATH> --max-colours <MAX-COLOURS>
+```
+
+rather than
+
+```
+dominant_colours <path> --max-colours <max-colours>
+```
+
+## v1.0.0 - 2021-11-27
+
+Initial release.
---
+++ dominant_colours/src/main.rs
@@ -0,0 +1,284 @@
+#![deny(warnings)]
+
+#[macro_use]
+extern crate clap;
+
+use kmeans_colors::get_kmeans_hamerly;
+use palette::{FromColor, IntoColor, Lab, Pixel, Srgb, Srgba};
+
+mod cli;
+mod get_bytes;
+
+fn main() {
+    let matches = cli::app().get_matches();
+
+    let path = matches
+        .get_one::<String>("PATH")
+        .expect("`path` is required");
+
+    let max_colours: usize = *matches
+        .get_one::<usize>("MAX-COLOURS")
+        .expect("`max-colours` is required");
+
+    // There's different code for fetching bytes from GIF images because
+    // GIFs are often animated, and we want a selection of frames.
+    let img_bytes = if path.to_lowercase().ends_with(".gif") {
+        get_bytes::get_bytes_for_gif(&path)
+    } else {
+        get_bytes::get_bytes_for_image(&path)
+    };
+
+    // This is based on code from the kmeans-colors binary, but with a bunch of
+    // the options stripped out.
+    // See https://github.com/okaneco/kmeans-colors/blob/0.5.0/src/bin/kmeans_colors/app.rs
+    let lab: Vec<Lab> = Srgba::from_raw_slice(&img_bytes)
+        .iter()
+        .map(|x| x.into_format::<_, f32>().into_color())
+        .collect();
+
+    let max_iterations = 20;
+    let converge = 1.0;
+    let verbose = false;
+    let seed: u64 = 0;
+
+    let result = get_kmeans_hamerly(max_colours, max_iterations, converge, verbose, &lab, seed);
+
+    let rgb = &result
+        .centroids
+        .iter()
+        .map(|x| Srgb::from_color(*x).into_format())
+        .collect::<Vec<Srgb<u8>>>();
+
+    // This uses ANSI escape sequences and Unicode block elements to print
+    // a palette of hex strings which are coloured to match.
+    // See https://alexwlchan.net/2021/04/coloured-squares/
+    for c in rgb {
+        let display_value = format!("#{:02x}{:02x}{:02x}", c.red, c.green, c.blue);
+
+        if matches.get_flag("no-palette") {
+            println!("{}", display_value);
+        } else {
+            println!(
+                "\x1B[38;2;{};{};{}m▇ {}\x1B[0m",
+                c.red, c.green, c.blue, display_value
+            );
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+
+    use assert_cmd::assert::OutputAssertExt;
+    use assert_cmd::Command;
+
+    // Note: for the purposes of these tests, I mostly trust the k-means code
+    // provided by the external library.
+
+    #[test]
+    fn it_prints_the_color_with_ansi_escape_codes() {
+        let output = get_success(&["./src/tests/red.png", "--max-colours=1"]);
+
+        assert_eq!(output.exit_code, 0);
+
+        assert!(
+            output.stdout == "\u{1b}[38;2;255;0;0m▇ #ff0000\u{1b}[0m\n"
+                || output.stdout == "\u{1b}[38;2;254;0;0m▇ #fe0000\u{1b}[0m\n",
+            "stdout = {:?}",
+            output.stdout
+        );
+
+        assert_eq!(output.stderr, "");
+    }
+
+    #[test]
+    fn it_can_look_at_png_images() {
+        let output = get_success(&["./src/tests/red.png", "--max-colours=1"]);
+        assert_eq!(output.exit_code, 0);
+    }
+
+    #[test]
+    fn it_can_look_at_jpeg_images() {
+        let output = get_success(&["./src/tests/noise.jpg", "--max-colours=1"]);
+        assert_eq!(output.exit_code, 0);
+    }
+
+    #[test]
+    fn it_can_look_at_static_gif_images() {
+        let output = get_success(&["./src/tests/yellow.gif", "--max-colours=1"]);
+        assert_eq!(output.exit_code, 0);
+    }
+
+    #[test]
+    fn it_can_look_at_tiff_images() {
+        let output = get_success(&["./src/tests/green.tiff", "--max-colours=1"]);
+        assert_eq!(output.exit_code, 0);
+    }
+
+    #[test]
+    fn it_omits_the_escape_codes_with_no_palette() {
+        let output = get_success(&["./src/tests/red.png", "--max-colours=1", "--no-palette"]);
+
+        assert_eq!(output.exit_code, 0);
+
+        assert!(
+            output.stdout == "#ff0000\n" || output.stdout == "#fe0000\n",
+            "stdout = {:?}",
+            output.stdout
+        );
+
+        assert_eq!(output.stderr, "");
+    }
+
+    #[test]
+    fn it_defaults_to_five_colours() {
+        let output = get_success(&["./src/tests/noise.jpg"]);
+
+        assert_eq!(
+            output.stdout.matches("\n").count(),
+            5,
+            "stdout = {:?}",
+            output.stdout
+        );
+    }
+
+    #[test]
+    fn it_lets_you_choose_the_max_colours() {
+        let output = get_success(&["./src/tests/noise.jpg", "--max-colours=8"]);
+
+        assert_eq!(
+            output.stdout.matches("\n").count(),
+            8,
+            "stdout = {:?}",
+            output.stdout
+        );
+    }
+
+    // The image created in the next two tests was created with the
+    // following command:
+    //
+    //      convert -delay 200 -loop 10 -dispose previous red.png blue.png red.png blue.png red.png blue.png red.png blue.png animated_squares.gif
+    //
+
+    #[test]
+    fn it_looks_at_multiple_frames_in_an_animated_gif() {
+        let output = get_success(&["./src/tests/animated_squares.gif"]);
+
+        assert_eq!(
+            output.stdout.matches("\n").count(),
+            2,
+            "stdout = {:?}",
+            output.stdout
+        );
+    }
+
+    #[test]
+    fn it_looks_at_multiple_frames_in_an_animated_gif_uppercase() {
+        let output = get_success(&["./src/tests/animated_upper_squares.GIF"]);
+
+        assert_eq!(
+            output.stdout.matches("\n").count(),
+            2,
+            "stdout = {:?}",
+            output.stdout
+        );
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_an_invalid_max_colours() {
+        let output = get_failure(&["./src/tests/red.png", "--max-colours=NaN"]);
+
+        assert_eq!(output.exit_code, 2);
+        assert_eq!(output.stdout, "");
+        assert_eq!(
+            output.stderr,
+            "error: Invalid value 'NaN' for '--max-colours <MAX-COLOURS>': invalid digit found in string\n\nFor more information try '--help'\n"
+        );
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_an_nonexistent_file() {
+        let output = get_failure(&["./doesnotexist.jpg"]);
+
+        assert_eq!(output.exit_code, 1);
+        assert_eq!(output.stdout, "");
+        assert_eq!(output.stderr, "No such file or directory (os error 2)\n");
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_an_nonexistent_gif() {
+        let output = get_failure(&["./doesnotexist.gif"]);
+
+        assert_eq!(output.exit_code, 1);
+        assert_eq!(output.stdout, "");
+        assert_eq!(output.stderr, "No such file or directory (os error 2)\n");
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_a_non_image_file() {
+        let output = get_failure(&["./README.md"]);
+
+        assert_eq!(output.exit_code, 1);
+        assert_eq!(output.stdout, "");
+        assert_eq!(
+            output.stderr,
+            "The file extension `.\"md\"` was not recognized as an image format\n"
+        );
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_an_unsupported_image_format() {
+        let output = get_failure(&["./src/tests/purple.webp"]);
+
+        assert_eq!(output.exit_code, 1);
+        assert_eq!(output.stdout, "");
+        assert_eq!(output.stderr, "The image format WebP is not supported\n");
+    }
+
+    #[test]
+    fn it_fails_if_you_pass_a_malformed_image() {
+        let output = get_failure(&["./src/tests/malformed.txt.png"]);
+
+        assert_eq!(output.exit_code, 1);
+        assert_eq!(output.stdout, "");
+        assert_eq!(
+            output.stderr,
+            "Format error decoding Png: Invalid PNG signature.\n"
+        );
+    }
+
+    struct DcOutput {
+        exit_code: i32,
+        stdout: String,
+        stderr: String,
+    }
+
+    fn get_success(args: &[&str]) -> DcOutput {
+        let mut cmd = Command::cargo_bin("dominant_colours").unwrap();
+        let output = cmd
+            .args(args)
+            .unwrap()
+            .assert()
+            .success()
+            .get_output()
+            .to_owned();
+
+        DcOutput {
+            exit_code: output.status.code().unwrap(),
+            stdout: str::from_utf8(&output.stdout).unwrap().to_owned(),
+            stderr: str::from_utf8(&output.stderr).unwrap().to_owned(),
+        }
+    }
+
+    fn get_failure(args: &[&str]) -> DcOutput {
+        let mut cmd = Command::cargo_bin("dominant_colours").unwrap();
+        let output = cmd.args(args).unwrap_err().as_output().unwrap().to_owned();
+
+        DcOutput {
+            exit_code: output.status.code().unwrap(),
+            stdout: str::from_utf8(&output.stdout).unwrap().to_owned(),
+            stderr: str::from_utf8(&output.stderr).unwrap().to_owned(),
+        }
+    }
+}
